5cf2d6b4b8e6cfd1f4cf5f6ebb89066bca41a471
rmmelfi <rmmelfi@gmail.com>
2022-09-27 20:04:27 -0400

    third commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -7,6 +7,7 @@ public class class03 {
 		
 		System.out.println("hey this is a git class");
 		System.out.println("hey this is a change in my project");
+		System.out.println("this is an amazing class");
 
 	}
 
